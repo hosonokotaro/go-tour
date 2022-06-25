e02518a4dfa965823331d7e5ddb199da48863526
hosonokotaro <hosono1985@gmail.com>
2022-06-25 23:26:57 +0900

    chore: Remove comment

--- main.go
+++ main.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"fmt"
-	// "math/rand"
-	// "math"
-	// "time"
 )
 
 func main() {
-	// ランダム値を unixtime で初期化する
-	// rand.Seed(time.Now().UnixNano())
-
-	// 分割代入の例
-	// a, b := swap("hello", "world")
-
 	a, b := split(17)
 
 	fmt.Println("a:", a)
 	fmt.Println("b:", b)
+
+	fmt.Println(swap("hello", "world"))
+	fmt.Println(add(3, 7))
 }
 
 // return 値に名前を付けると、変数宣言と同様の効果を得る。この return は、戻り値すべてを返す
