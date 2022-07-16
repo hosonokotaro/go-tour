8b668e8f74238642a98eb6f8692745db078c7015
hosono kotaro <hosono1985@gmail.com>
2022-07-16 17:51:05 +0900

    lesson1: pointer 入門

--- main.go
+++ main.go
@@ -5,28 +5,14 @@ import (
 )
 
 func main() {
-	a, b := split(17)
+	var a int = 42
+	var b *int = &a
 
-	fmt.Println("a:", a)
-	fmt.Println("b:", b)
+	fmt.Println("pointer はアドレスを受け取る", a, b)
+	fmt.Println("pointer を値として受け取る", a, *b)
 
-	fmt.Println(swap("hello", "world"))
-	fmt.Println(add(3, 7))
-}
-
-// return 値に名前を付けると、変数宣言と同様の効果を得る。この return は、戻り値すべてを返す
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
-// return に複数の変数を指定できる、取り出すときはどうするかまだわからない
-func swap(a, b string) (string, string) {
-	return b, a
-}
-
-// まとめて型を指定するとき
-func add(x, y int) int {
-	return x + y
+	a = 27
+	fmt.Println("a を変更する", a, *b)
+	*b = 35
+	fmt.Println("b を変更する", a, *b)
 }
