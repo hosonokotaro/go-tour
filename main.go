cd2b2090bd7eeedf6314e092633b5a991db6651f
hosono kotaro <hosono1985@gmail.com>
2022-07-17 11:55:35 +0900

    lesson2: 関数に pointer を渡す

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	for i := 0; i < 5; i++ {
-		func(j int) {
-			fmt.Println(j)
-		}(i)
+	g := greeter{
+		greeting: "Hello",
+		name:     "Taro",
 	}
+	fmt.Println("geeter struct を実装した g", g)
+
+	g.greet()
+	fmt.Println("name を変更した g", g)
+}
+
+type greeter struct {
+	greeting string
+	name     string
+}
+
+// NOTE: *greeter は pointer として受け取り、その参照を変数内で変更している例
+// もし、pointer ではなく value を渡した場合はコストがかかることを注意される
+func (g *greeter) greet() {
+	fmt.Println("g の struct はここに渡されている。greet() は main 関数から実行できる", g.greeting, g.name)
+	g.name = "幽霊"
 }
